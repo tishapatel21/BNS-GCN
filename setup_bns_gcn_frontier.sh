6234d7c7cf5fb7105f95b2b693425d2a46223a79
Tisha Patel <tpatel21@terpmail.umd.edu>
2026-03-02 13:32:50 -0500

    add script

---
+++ setup_bns_gcn_frontier.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+WRKSPC=$SCRATCH/BNS-GCN
+# everything will be installed in $WRKSPC
+
+ENV_NAME="venv"
+# this is the name of your python venv, change if needed
+
+cd $WRKSPC || exit 1
+
+module purge 
+module load PrgEnv-amd
+module load rocm
+module load cray-mpich
+module load miniforge3
+
+echo -e "${RED}Creating Python Environment in $WRKSPC:${GREEN}"
+python -m venv $WRKSPC/$ENV_NAME
+source $WRKSPC/$ENV_NAME/bin/activate
+pip install --upgrade pip
+
+
+pip install torch torchvision --index-url https://download.pytorch.org/whl/rocm5.7
+
+pip install -r requirements.txt
+
+python -c "import torch; print(torch.__version__, torch.version.hip, torch.cuda.is_available())"
