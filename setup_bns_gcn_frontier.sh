01012e7e7e6724fe7efbc19d9527d185fbbf8c7e
Tisha Patel <tpatel21@terpmail.umd.edu>
2026-03-04 22:54:29 -0500

    different version of python

--- setup_bns_gcn_frontier.sh
+++ setup_bns_gcn_frontier.sh
@@ -20,7 +20,7 @@ module load miniforge3
 
 echo -e "${RED}Creating Python Environment in $WRKSPC:${GREEN}"
 rm -rf $WRKSPC/$ENV_NAME
-python -m venv $WRKSPC/$ENV_NAME
+python3.10 -m venv $WRKSPC/$ENV_NAME
 source $WRKSPC/$ENV_NAME/bin/activate
 pip install --upgrade pip
 
@@ -30,8 +30,6 @@ pip install scipy
 pip install -r requirements.txt --no-deps
 
 pip install https://pypi.amd.com/rocm-7.0.0/packages/amd-dgl/amd_dgl-2.4.0+amd0.torch2.8.0.rocm7.0.0.git64359f59.ubuntu24.4-cp312-cp312-linux_x86_64.whl
-
-cd dgl
 
 cd $WRKSPC
 
