644e5414fdd4e4ac3bcb03db17ee68684cf256a6
Tisha Patel <tpatel21@terpmail.umd.edu>
2026-03-05 00:32:01 -0500

    pyyaml

--- setup_bns_gcn_frontier.sh
+++ setup_bns_gcn_frontier.sh
@@ -24,12 +24,13 @@ python3.10 -m venv $WRKSPC/$ENV_NAME
 source $WRKSPC/$ENV_NAME/bin/activate
 pip install --upgrade pip
 
-pip install torch==2.3.1+rocm5.7 torchvision==0.18.1+rocm5.7 --index-url https://download.pytorch.org/whl/rocm5.7
+pip install torch==2.3.1+rocm5.7 torchvision==0.15.2+rocm5.7 --index-url https://download.pytorch.org/whl/rocm5.7
 
 pip install torchdata==0.7.0
 pip install pandas>=1.5 scikit-learn>=1.2 six>=1.16 outdated>=0.2.2 tqdm requests scipy
 pip install ogb==1.3.6
 pip install dgl==2.1.0
+pip install pyyaml
 pip install -r requirements.txt --no-deps
 
 cd $WRKSPC
