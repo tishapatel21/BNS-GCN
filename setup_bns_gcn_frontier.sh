00561b657aef9b7fb1d32221d1bb274badd9f7fd
Tisha Patel <tpatel21@terpmail.umd.edu>
2026-03-05 00:37:32 -0500

    pydantic

--- setup_bns_gcn_frontier.sh
+++ setup_bns_gcn_frontier.sh
@@ -24,6 +24,8 @@ python3.10 -m venv $WRKSPC/$ENV_NAME
 source $WRKSPC/$ENV_NAME/bin/activate
 pip install --upgrade pip
 
+pip install "numpy<2"
+
 pip install torch==2.3.1+rocm5.7 torchvision==0.15.2+rocm5.7 --index-url https://download.pytorch.org/whl/rocm5.7
 
 pip install torchdata==0.7.0
@@ -31,6 +33,7 @@ pip install pandas>=1.5 scikit-learn>=1.2 six>=1.16 outdated>=0.2.2 tqdm request
 pip install ogb==1.3.6
 pip install dgl==2.1.0
 pip install pyyaml
+pip install pydantic
 pip install -r requirements.txt --no-deps
 
 cd $WRKSPC
